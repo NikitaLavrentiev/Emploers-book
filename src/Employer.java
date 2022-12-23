7870f66be0c45b0dfd2be5ba044e0a49eab17a78
Никита <lavrentivev.nikita@gmail.com>
2022-12-23 22:01:56 +0300

    created class Employer

--- src/Employer.java
+++ src/Employer.java
@@ -1,19 +1,26 @@
 public class Employer {
-            String surnameEmployer;
-            String nameEmployer;
-            String patronymicEmployer;
-            String employeeDepartment;
-            int salaryEmployer;
+    private final String surnameEmployer;
+    private final String nameEmployer;
+    private final String patronymicEmployer;
+    private final String employeeDepartment;
+    private final int salaryEmployer;
+    public static int id; //мб public и Employer.id ниже
 
-
-    public Employer (String surname, String name, String patronymic, String department, int salary ) {
+    public Employer(String surname, String name, String patronymic, String department, int salary) {
         this.surnameEmployer = surname;
         this.nameEmployer = name;
         this.patronymicEmployer = patronymic;
         this.employeeDepartment = department;
         this.salaryEmployer = salary;
-
+        //добавить счётчик
     }
 
-
+    public int getId() {
+        while (id = 0) {
+            id =1;
+            return 0;
+        } return id++;
+    }
 }
+
+
